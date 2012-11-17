1a2851354f583987de5544973a7484a4fc54a6e8
jden <jason@denizac.org>
2012-11-16 19:55:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# monget
+a commandline tool to run mongo queries to stdout
+
+## Installation
+
+    npm install -g monget
+
+## Usage
+
+    $ node monget --conn <connection string> <mongo shell javascript command>
+
+Options:
+
+  -c, --conn  a mongodb connection string (see http://www.mongodb.org/display/DOCS/Connections)
+  -s, --save  save the connection to a .monget file in the current directory                     [boolean]
+
+Example:
+
+    monget --conn mongodb://localhost/myDB "db.users.findOne()" > user.json
+
+monget requires a mongodb connection string. From the mongo docs:
+
+    mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+
+eg
+
+    mongodb://localhost/myDb
+
+If no connection string is passed in the `--conn` argument, monget will check for a connection string in a file called `.monget`. You can call monget with the `--save` flag to save the connection string argument to the .monget file for convenience.
+
+## License
+Written by Jason Denizac <jason@denizac.org>
+
+Copyright 2012 Agile Diagnosis, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ monget.js
@@ -0,0 +1,110 @@
+var opt = require('optimist');
+var fs = require('fs');
+var url = require('url');
+var spawn = require('child_process').spawn;
+
+var opt = opt
+	.usage('Usage: $0 [--conn] "mongoshell javascript"')
+	.demand(['_'])
+	.alias('c','conn')
+	.describe('c','a mongodb connection string (see http://www.mongodb.org/display/DOCS/Connections)')
+	.boolean('s')
+	.alias('s','save')
+	.describe('s','save the connection to a .monget file in the current directory');
+
+
+getConnectionString(function (err, connectionString) {
+	if (err) return onError(err);
+
+	var connectionInfo = parseMongoConnectionString(connectionString);
+
+	runMongoCommand(connectionInfo, opt.argv._);
+});
+
+
+//requires mongoshell 2.0.x
+
+function runMongoCommand(connectionInfo, cmd, cb) {
+
+	var args = [];
+
+	args.push(formatConnectionInfoForMongoShell(connectionInfo));
+
+	if (connectionInfo.username)
+		args.push('-u', connectionInfo.username);
+
+	if (connectionInfo.pass)
+		args.push('-p', connectionInfo.pass);
+
+	args.push('--quiet');
+	args.push('--eval', 'printjson(' + cmd + ')');
+
+	var mongo = spawn('mongo', args);
+	mongo.stdout.pipe(process.stdout);
+	mongo.stderr.pipe(process.stderr);
+}
+
+
+function getConnectionString(cb) {
+	var file = '.monget';
+	if (opt.argv.conn) {
+		if (opt.argv.save) {
+			fs.writeFile(file, opt.argv.conn, function (err) {
+				cb(err, opt.argv.conn);
+			});
+		} else {
+			cb(null, opt.argv.conn);
+		}
+	} else {
+
+		fs.exists(file, function (exists) {
+			if (!exists) {
+				return cb('.monget file not found and no --conn specified!');
+			}
+			fs.readFile(file, 'utf8', cb);
+		});
+	}
+}
+
+function parseMongoConnectionString(str) {
+	var cs = url.parse(str);
+	if (cs.protocol !== 'mongodb:') {
+		onError(new Error('connectionString protocol must be `mongodb:`'));
+	}
+	if (!cs.hostname) {
+		onError(new Error('connectionString must specify a hostname'));
+	}
+	if (!cs.path) {
+		onError(new Error('connectionString must specify a db name in the URL path'));
+	}
+	var username, pass;
+	if (cs.auth) {
+		cs.auth = cs.auth.split(':');
+		username = cs.auth[0];
+		pass = cs.auth[1];
+	}
+	return {
+		host: cs.hostname,
+		port: parseInt(cs.port),
+		db: cs.path.substr(1),
+		username: username,
+		pass: pass
+	};
+}
+
+function formatConnectionInfoForMongoShell(connectionInfo) {
+	var str = connectionInfo.host;
+	if (connectionInfo.port) {
+		str = str + ':' + connectionInfo.port;
+	}
+	if (connectionInfo.db) {
+		str = str + '/' + connectionInfo.db;
+	}
+	return str;
+}
+
+function onError(err) {
+	opt.showHelp();
+	console.error(err.message);
+	process.exit(1);
+}
